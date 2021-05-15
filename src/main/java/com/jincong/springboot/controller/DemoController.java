8bc56f6dedc38df9b6744877f48cd4c9ea827099
jjcong <jincong00@163.com>
2021-05-15 14:23:04 +0800

    新增自定义starter

--- src/main/java/com/jincong/springboot/controller/DemoController.java
+++ src/main/java/com/jincong/springboot/controller/DemoController.java
@@ -1,8 +1,11 @@
 package com.jincong.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import service.HelloService;
 
 /**
  * DemoController
@@ -13,12 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @ResponseBody
+@RequestMapping("/demo")
 public class DemoController {
+
+    @Autowired
+    HelloService helloService;
 
 
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello World!";
+        return helloService.sayHello("Hello");
 
     }
 
